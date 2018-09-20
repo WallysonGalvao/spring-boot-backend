9fa1ed0dd2f08735cd49abb8a091a952b0c461c1
Wallyson Galvão <wallyson.galvao@gmail.com>
2018-09-20 11:58:31 -0300

    Email HTML

--- src/main/java/com/nelioalves/cursomc/services/EmailService.java
+++ src/main/java/com/nelioalves/cursomc/services/EmailService.java
@@ -2,6 +2,8 @@ package com.nelioalves.cursomc.services;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import javax.mail.internet.MimeMessage;
+
 import com.nelioalves.cursomc.domain.Cliente;
 import com.nelioalves.cursomc.domain.Pedido;
 
@@ -11,5 +13,10 @@ public interface EmailService {
 
 	void sendEmail(SimpleMailMessage msg);
 
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
+
 	void sendNewPasswordEmail(Cliente cliente, String newPass);
+
 }
